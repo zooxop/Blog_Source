23dbad3c36882d7e776d8b8296f5f7de8b85b414
zooxop <zooxop@gmail.com>
2020-07-01 01:12:32 +0900

    Added Comment on my own usage.

--- FreeAndNil_Test/uFreeAndNil_Test.pas
+++ FreeAndNil_Test/uFreeAndNil_Test.pas
@@ -49,24 +49,33 @@ var
 begin
   myStringList := TStringList.Create;
 
-
-  //FreeAndNil(myStringList);
-
   myStringList.Add('Item1');
   myStringList.Add('Item2');
 
   myStringList.free;
 
+  //Check the object is freed.
   if PULONG_PTR(PByte(myStringList) - sizeof(ULONG_PTR))^ and 1 <> 0 then
     showmessage('myStringList is Freed.');
 
-  myStringList.Add('Item3');
+  myStringList.Add('Item3');  //This line is not create error.
 
+  //ReCheck but It is still free
   if PULONG_PTR(PByte(myStringList) - sizeof(ULONG_PTR))^ and 1 <> 0 then
     showmessage('myStringList is Freed.');
 
-  ShowMessage(myStringList[0]);
+  ShowMessage(myStringList[0]);    //message : "Item3"
 
+  FreeAndNil(myStringList);
+
+  myStringList.Add('Item4');  //This line is create Error : "Invalid pointer operation."
+
+  //myStringList 의 메모리는 해제되었지만, 여전히 TStringList 로서의 기능은 작동을 한다. (Free를 해줬음에도, Add() 가 작동함.)
+  //하지만 Free 시켜주기 전에 Add 해주었던 Item 2개는 초기화 되었음을 확인할 수 있다. (myStringList[0] 을 출력했을 때, "Item3" 이 출력됨.)
+  //FreeAndNil 을 호출한 뒤 Add 를 하면 Runtime Error가 발생하는것을 확인할 수 있다.
+  //-> 객체가 nil을 point 하도록 FreeAndNil 이 처리해주었기 때문에, 더이상 TStirngList 로서의 작동을 할 수 없다.
+  //--> 따라서 Free 만을 사용하는 경우, 런타임 에러는 없지만 개발자가 예상하지 못한 에러가 발생할 가능성이 커질 것으로 생각한다.
+  //---> 가급적이면 FreeAndNil 을 사용하여 메모리를 해제하고, 객체를 사용하기 전에는 Assigned(TObject) Function을 활용하여 런타임 에러를 피하자.
 end;
 
 procedure TFormFreeAndNil.ButtonExecTwiceClick(Sender: TObject);
