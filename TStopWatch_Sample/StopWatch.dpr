56e48cabcd2c51c80b733ebb42e607bdc51cb0fd
CheolHyun Mun <zooxop@gmail.com>
2021-12-14 10:51:08 +0900

    missed .dpr files

---
+++ TStopWatch_Sample/StopWatch.dpr
@@ -0,0 +1,14 @@
+program StopWatch;
+
+uses
+  Vcl.Forms,
+  StopWatchSample in 'StopWatchSample.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
